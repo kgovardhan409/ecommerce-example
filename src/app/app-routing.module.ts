5aea6e4e77f9e4aa35e95d04e6e9f3a659e2cc5c
surendhar483 <surendhars1234@gmail.com>
2023-01-07 08:36:39 +0530

    header and banner changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './banner/banner.component';
 import { BrandsComponent } from './brands/brands.component';
 import {CartComponent} from './cart/cart.component';
+import { HeaderComponent } from './header/header.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 
 const routes: Routes = [
   {path: '', component:BrandsComponent},
   {path: 'brands', component: BrandsComponent},
   {path: 'cart', component: CartComponent},
+  {path:'sign-up',component:SignUpComponent}
 ];
 
 @NgModule({
